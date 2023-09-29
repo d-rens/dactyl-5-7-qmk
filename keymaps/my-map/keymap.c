325ab1d65811602f56dd37df30087da0098436e8
d-rens <displscord@gmail.com>
2023-09-29 17:12:52 +0200

    Updated Layout

--- keymaps/my-map/keymap.c
+++ keymaps/my-map/keymap.c
@@ -23,8 +23,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,    KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   
     KC_LCTL,    KC_LGUI, TAB_L,   TAB_R,
                                                   KC_LSFT, KC_SPC,
-                                                  KC_A   , KC_HOME, // changes with row under
-                                                  KC_LGUI, KC_LALT, // changes with row above 'a' does not work
+                                                  KC_LALT, KC_TAB,   
+                                                  KC_LGUI, KC_PGDN,
     // right hand
     KC_7,       KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_GRV,
     KC_RBRC,    KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLS,
@@ -32,8 +32,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_N,    KC_M,    KC_COMM,  LT(_FN,KC_DOT),   KC_SLSH,  KC_RSFT,
                          KC_LEFT, KC_DOWN,  KC_UP,    KC_RGHT,
     KC_BSPC,    KC_ENT,
-    KC_RALT,    KC_RCTL,
-    LT(_NUMPAD,KC_ESC),  KC_PGDN
+    KC_RALT,    LT(_NUMPAD,KC_ESC),
+    KC_PGUP,  KC_RCTL
 ),
 
 [_FN] = LAYOUT_5x7(
@@ -68,11 +68,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                 _______, _______,
                                 _______, _______,
     // right hand
-                      _______,   _______,   KC_NUM,    _______,   KC_PMNS,   KC_PPLS,   _______,
-                      _______,   _______,   KC_P7,     KC_P8,     KC_P9,     _______,   _______,
-                      _______,   _______,   KC_P4,     KC_P5,     KC_P6,     KC_PAST,   _______,
-                                 _______,   KC_P1,     KC_P2,     KC_P3,     KC_PSLS,   _______,
-                                                       KC_P0,     KC_PDOT,   _______,   _______,
+                      _______,   _______,   ____,     ____,     ____,     _______,   _______,
+                      _______,   _______,   KC_7,     KC_8,     KC_9,     _______,   _______,
+                      _______,   _______,   KC_4,     KC_5,     KC_6,     _______,   _______,
+                                 _______,   KC_1,     KC_2,     KC_3,     _______,   _______,
+                                                      KC_0,     KC_DOT,   _______,   _______,
          _______, KC_PENT,
          _______, _______,
          _______, _______
