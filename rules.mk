dcc7061ab8a4a5c53453871446cdf6178c467ee6
d-rens <displscord@gmail.com>
2024-04-21 17:41:41 +0200

    Enabled VIA

--- rules.mk
+++ rules.mk
@@ -2,9 +2,10 @@
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
+#MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = yes         # Console for debug
+#CONSOLE_ENABLE = yes        # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 SPLIT_KEYBOARD = yes
 UNICODE_ENABLE = yes
+VIA_ENABLE = yes
