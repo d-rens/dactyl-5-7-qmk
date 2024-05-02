f2b890327d7d100410e6f0a14633baccf71b7871
d-rens <displscord@gmail.com>
2024-05-02 14:58:22 +0200

    Removed duplicate, sorted

--- rules.mk
+++ rules.mk
@@ -1,12 +1,11 @@
 # Build Options
 #   change yes to no to disable
-#
-BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-#MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
+
 #CONSOLE_ENABLE = yes        # Console for debug
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 COMMAND_ENABLE = yes        # Commands for debug and configuration
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+MOUSEKEY_ENABLE = yes
 SPLIT_KEYBOARD = yes
 UNICODE_ENABLE = yes
 VIA_ENABLE = yes
-MOUSEKEY_ENABLE = yes
